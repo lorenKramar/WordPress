9563af946edf00d100113e4b0823a6da14c65a1e
Maren Miller <Maren@Josephs-MacBook-Air.local>
2012-09-20 15:49:45 -0400

    Adding my special widget for displaying recent Party Pirate posts - to be added to the sidebar - PLUS the official SoundCloud shortcode widget

---
+++ wp-content/plugins/recent-post-type.php
@@ -0,0 +1,60 @@
+
+<?php
+/*
+Plugin Name: Recent Party Pirates Posts
+Description: Random Post Widget grabs a random post and the associated thumbnail to display on your sidebar
+Author: James Bruce
+Version: 1
+Author URI: http://jamesbruce.me/
+*/
+ 
+ 
+class RecentPartyPiratePost extends WP_Widget
+{
+  function RecentPartyPiratePost()
+  {
+    $widget_ops = array('classname' => 'RecentPartyPiratePost', 'description' => 'Displays Recent Party Pirates Posts' );
+    $this->WP_Widget('RecentPartyPiratePost', 'Recent Party Pirate Post', $widget_ops);
+  }
+ 
+  function form($instance)
+  {
+    $instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+    $title = $instance['title'];
+?>
+  <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+<?php
+  }
+ 
+  function update($new_instance, $old_instance)
+  {
+    $instance = $old_instance;
+    $instance['title'] = $new_instance['title'];
+    return $instance;
+  }
+ 
+  function widget($args, $instance)
+  {
+    extract($args, EXTR_SKIP);
+ 
+    echo $before_widget;
+    $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+ 
+    if (!empty($title))
+      echo $before_title . $title . $after_title;;
+ 
+    // WIDGET CODE GOES HERE
+    
+
+      $args = array( 'numberposts' => '10', 'post_type' => 'party_pirates' );
+      $recent_posts = wp_get_recent_posts( $args );
+      foreach( $recent_posts as $recent ){
+        echo '<h6><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </h6> ' . '<div class="excerpt">' . '&ldquo;' . $recent["post_excerpt"] . '&rdquo;' .'</div>';
+      }
+
+ 
+    echo $after_widget;
+  }
+ 
+}
+add_action( 'widgets_init', create_function('', 'return register_widget("RecentPartyPiratePost");') );?>
